5a5a33e16b04ed6d1debc089aabad15a23db671d
Eduardo Lacerda <63383406+dudulacerdadl@users.noreply.github.com>
2023-02-09 00:52:16 +0000

    Realizada atividade 2 do primeiro dia

--- 01-advent-of-code-challenge/01/solution_part_ii.py
+++ 01-advent-of-code-challenge/01/solution_part_ii.py
@@ -1,17 +1,22 @@
-def get_first_tree_max_group(file):
-    lines = file.readlines()
-    elvs = []
-    aux_elv = []
-    for line in lines:
-        if line == '\n':
-            elvs.append(sum(aux_elv))
-            aux_elv = []
-            continue
-        line = int(line.split('\n')[0])
-        aux_elv.append(line)
+class PartII:
+    def __init__(self, file):
+        self.file = file
 
-    elvs.sort(reverse=True)
-    return sum([elvs[0], elvs[1], elvs[2]])
+    def get_first_tree_max_group(self):
+        lines = self.file.readlines()
+        elvs = []
+        aux_elv = []
+        for line in lines:
+            if line == '\n':
+                elvs.append(sum(aux_elv))
+                aux_elv = []
+                continue
+            line = int(line.split('\n')[0])
+            aux_elv.append(line)
+
+        elvs.sort(reverse=True)
+        return sum([elvs[0], elvs[1], elvs[2]])
 
 
-print(get_first_tree_max_group(open("sample.in")))
+part_ii = PartII(open("sample.in"))
+print(part_ii.get_first_tree_max_group())
