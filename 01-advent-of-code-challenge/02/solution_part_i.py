a207e810d9bfd6bdfde1224e21bc6fd8234ea197
Eduardo Lacerda <63383406+dudulacerdadl@users.noreply.github.com>
2023-02-14 23:30:42 +0000

    Adicionada solução do dia 2, parte 1

--- 01-advent-of-code-challenge/02/solution_part_i.py
+++ 01-advent-of-code-challenge/02/solution_part_i.py
@@ -1,23 +1,40 @@
-# A = Pedra
-# B = Papel
-# C = Tesoura
-# ---
-# X = Papel -> 2
-# Y = Pedra -> 1
-# Z = Tesoura -> 3
-# ---
-# Derrota = 0
-# Empate = 3
-# Vitória = 6
-
 class PartI:
     def __init__(self, file):
         self.file = file
 
     def get_result_points(self):
         lines = self.file.readlines()
+        result = 0
         for line in lines:
-            if line == '\n':
-                continue
-            line = int(line.split('\n')[0])
-            game = []
+            oponent = line[0]
+            your = line[2]
+
+            if your == 'X':
+                result += 1
+
+            if your == 'Y':
+                result += 2
+
+            if your == 'Z':
+                result += 3
+
+            result += self.combat(oponent, your)
+
+        return result
+
+    def combat(self, oponent, your):
+        result = 0
+        if (oponent == 'A' and your == 'Z') or (oponent == 'B' and your == 'X') or (oponent == 'C' and your == 'Y'):
+            result = 0
+
+        if (oponent == 'A' and your == 'X') or (oponent == 'B' and your == 'Y') or (oponent == 'C' and your == 'Z'):
+            result = 3
+
+        if (oponent == 'A' and your == 'Y') or (oponent == 'B' and your == 'Z') or (oponent == 'C' and your == 'X'):
+            result = 6
+
+        return result
+
+
+part_i = PartI(open("input.txt"))
+print(part_i.get_result_points())
